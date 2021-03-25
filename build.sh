1854b95378a80e695844a7ceda63f5276e06a1dc
Ladislav Louka <loukalad@fit.cvut.cz>
2021-03-25 16:41:40 +0100

    Add empty templates folder, remove the .gitkeep in build
    Plus documentation of build script

--- build.sh
+++ build.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
 
+# Clean previous run
 rm -r build
 cd src/react
+# run npm in respective folder
 npm install
+# React build and make into django template
 npm run djangofy
+# return home
 cd ../..
-mkdir build
 
-cp -ar src/python/. build
-cp -ar src/react/build/static build/simple_api_admin/static
-cp -a src/react/build/index.html build/simple_api_admin/templates/index.html
+# New build
+mkdir build
+# Copy python module part
+cp -avr src/python/. build
+# Copy react build static files
+cp -avr src/react/build/static build/simple_api_admin/static
+# Copy template file
+cp -av src/react/build/index.html build/simple_api_admin/templates/index.html
+# Remove unnecessary .gitkeep
+rm build/simple_api_admin/templates/.gitkeep
